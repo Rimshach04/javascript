c68316a061cfa999a56e5c9c4aae8d0ca0943d33
RimshaChoudary <rimshachoudhary.7614@gmail.com>
2023-06-12 22:32:35 +0500

    javascript veriables

---
+++ veriables.js
@@ -0,0 +1,57 @@
+console.log("veriables");
+// how many type of veriables in javascript 
+// there are three types of veriables in javascript
+
+
+// var : it is redeclareable( mean redeclare varible) and reassign value it can be outside any funcation
+// it show in a window object
+
+                      //    1
+//  var x=10
+//   console.log(x);
+                            // 2
+//  var x=10
+//  var  x=50;
+//  x=89;
+//  console.log(x);
+
+
+//  let : second variable is let it is use for a limited scope in a block.it con not be redeclareable
+// but reassign value .it is not show in window object
+
+
+            // 1
+// let a="rimsha";
+// console.log(a);
+
+             // 2
+// it show eror becouse let can not be redeclearable
+// let a=78;
+// let a=90;
+// console.log(a);
+
+            //  3
+// but reassign value
+// let a=50;
+// a=90;
+ // console.log(a);
+
+
+// const: it not reassign value and redeclare
+
+
+        //   1
+    // const c=45;
+    // console.log(c);
+        
+         // 2
+    // not redeclare
+    // const c=45;
+    // const c=89;
+    //  console.log(c);
+
+    // 3
+//  not reassign
+//  const c=45;
+//     c=89;
+//      console.log(c);
