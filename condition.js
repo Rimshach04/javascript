4bdf12063814c82eff12a860c420e038b1be18dd
RimshaChoudary <rimshachoudhary.7614@gmail.com>
2023-06-13 13:35:31 +0500

    all conditions here of javascript

---
+++ condition.js
@@ -0,0 +1,64 @@
+console.log(" condition.js");
+// console.log() ->is n output  funcation in js
+//there are five type of  conditions
+// if,if else,if elseif else ,switch, ternary
+          
+                                                   //    1 if
+
+// var a=45;
+// if(a>40){
+//     console.log("true");
+// }
+
+                                              // 2 if else
+                   
+// var a=25;
+// if(a%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("false");
+// }
+               
+                                      //  3 if else if
+
+// var age= 17;
+// if(age>=20){
+//     console.log("eligible for vote");
+// }
+// else if(age==17){
+//     console.log("eligible after one year");
+// }
+// else{
+//     console.log("not eligible")
+// }
+
+                                               // 4 switch
+
+// var x=4;
+// switch(x){
+//     case 1:{
+//         console.log("monday");
+//         break;
+//    }
+//    case 2:
+//     {
+//     console.log("tuesdy");
+//     break;
+// }
+//    case 3:{
+//     console.log("wednesday");
+//     break;
+// }
+// default:
+//     {
+//    console.log("nothing");
+//     break;
+//     }
+// }
+                                         //     5 ternary
+
+// var a=5;
+// var b;
+// b=a<10? "true":"false";
+// console.log(b);
