4dcfd12b2af79712f85d7a83f4f1517dfd81b2fd
RimshaChoudary <rimshachoudhary.7614@gmail.com>
2023-06-16 19:55:03 +0500

    funcions

---
+++ functions.js
@@ -0,0 +1,52 @@
+console.log("function");
+// type of function in javascript
+// there are 6 types of function
+
+
+                              //   1st        naming function
+
+// function show(){
+//     var a=20;
+//     var b=30;
+//     var c=a+b;
+//     console.log("sum of a and b is: "+c);
+// }
+// show();
+
+                        // 2nd   parameterize function
+
+        //  function sum(a,b){
+        //      console.log(a+b)
+        //      console.log(typeof sum);
+            // do this also
+        //     let c=a+b;
+        //     console.log(c);
+        //  }             
+        //  sum(4,5);
+
+
+            //   3rd  Anoyonmous function
+            //  a function with out name
+
+            // let Full_Name=function(Fname,Lname){
+            //         console.log(Fname,Lname);
+            // }
+            // Full_Name("rimsha","ch");
+
+            //    4th function    arrow function
+                //    method one
+
+        //    let sum=(a,b)=>
+        //     a>b?console.log("true"):console.log("false");
+        //     sum(4,5);
+
+            // method two
+            // let sum=(a,b)=>{
+            //     if(a>b){
+            //      console.log("a is greater");
+            //     }else{
+            //         console.log("b is greater");
+            //     }
+            // }
+            // sum(3,4);
+            
